f4d01f8ef7e762140da09e6bcf7dba63acc149f6
aleruz-03 <alessiaruzziconi@gmail.com>
2025-08-06 12:53:36 +0200

    Delete Progetto_IDS_24-25/src/main/java/org/example/app/model/IFileInformazioni.java

--- Progetto_IDS_24-25/src/main/java/org/example/app/model/IFileInformazioni.java
+++
@@ -1,6 +0,0 @@
-package org.example.app.model;
-//rappresenta i diversi tipidiinformazioni chesi possono avere
-public interface IFileInformazioni extends Messaggio{
-    String getContenuto();
-    String getNome();
-}
