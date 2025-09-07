d0a8b5a52f8bd3bf43cd5b0346bcfc715186f7bd
Chiara01piccinini <148806902+Chiara01piccinini@users.noreply.github.com>
2025-09-07 11:30:01 +0200

    Delete software-engineering-project/Persistence directory

--- software-engineering-project/Persistence/software-engineering-project/src/main/java/org/example/software_engineering_project/model/Messaggio.java
+++
@@ -1,7 +0,0 @@
-package org.example.software_engineering_project.model;
-
-// Rappresenta tutti gli oggetti scambiabili come informazioni tra gestori
-public interface Messaggio {
-    String getNome();
-    String getContenuto(); // Metodo comune per ottenere il contenuto rappresentativo
-}
